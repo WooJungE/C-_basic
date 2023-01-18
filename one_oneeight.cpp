b8479a610e620679448feb44c2b4f9be8edf8b37
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-01-18 15:59:48 +0900

    Add files via upload

---
+++ one_oneeight.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//인스턴스만 동적으로 생성하는 경우
+	//new 연산자는 메모리 크기를 정하지 않음
+	int* pData = new int;
+
+	//초깃값을 기술하는 경우
+	int* pNewData = new int(10);
+
+	*pData = 5;
+	std::cout << *pData << std::endl;
+	std::cout << *pNewData << std::endl;
+
+	delete pData;
+	delete pNewData;
+	//5
+	//10
+}
